cb49d06629ec22eb7e21eb5e70a6d80b1b4d1dff
Junior Safera <juniorsafera.dev@gmail.com>
2021-12-22 23:10:06 -0300

    criando_projeto

---
+++ whatsapp_web/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_web/telas/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "WhatsApp Web",
+    home: TelaLogin(),
+  ));
+}
+ 
---
+++ whatsapp_web/lib/telas/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TelaLogin extends StatefulWidget {
+  const TelaLogin({ Key? key }) : super(key: key);
+
+  @override
+  _TelaLoginState createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
