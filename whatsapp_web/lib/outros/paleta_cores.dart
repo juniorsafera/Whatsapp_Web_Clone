e4745d5f11c8d3387a6f64f3c3d6de55a11202b9
Junior Safera <juniorsafera.dev@gmail.com>
2021-12-23 23:29:29 -0300

    criando tela login

---
+++ whatsapp_web/lib/outros/paleta_cores.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaletaCores{
+
+    static const Color corFundo = Color(0xffD9DBD5);
+    static const Color corPrimaria = Color(0xff075E54);
+
+
+}
--- whatsapp_web/lib/telas/login.dart
+++ whatsapp_web/lib/telas/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_web/outros/paleta_cores.dart';
 
 class TelaLogin extends StatefulWidget {
   const TelaLogin({ Key? key }) : super(key: key);
@@ -8,10 +9,156 @@ class TelaLogin extends StatefulWidget {
 }
 
 class _TelaLoginState extends State<TelaLogin> {
+
+      TextEditingController _controllerNome  = TextEditingController(text: 'Junior Santos');
+      TextEditingController _controllerEmail = TextEditingController(text: 'junior@gmail.com');
+      TextEditingController _controllerSenha = TextEditingController(text: '1234567');
+      bool _cadastroUsuario = false;
+
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+    double larguraTela = MediaQuery.of(context).size.width;
+    double alturaTela = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        body: Container(
+          color: PaletaCores.corFundo,
+          width: larguraTela,
+          height: alturaTela,
+          child: Stack(
+            children: [
+
+              Positioned(child: 
+              Container(
+                width: larguraTela,
+                height: alturaTela * 0.4, // 40%
+                color: PaletaCores.corPrimaria,
+              )
+              ),
+
+              Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Card(
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10)
+                            )
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.all(40),
+                            width: 500,
+                            height: 400,
+                            child: Column(
+                              children: [
+                                  
+                                  // Imagem perfil com botão
+
+                                  Visibility(
+                                    visible: _cadastroUsuario,
+                                    child: 
+                                  ClipOval(
+                                      child: 
+                                      //Text("Teste"),
+                                      Icon(
+                                        Icons.account_circle,
+                                         
+                                        color: PaletaCores.corPrimaria,
+                                      )
+                                  ),
+                                  ),
+
+
+                                  // Caixa nome
+                                  Visibility(
+                                    visible: _cadastroUsuario,
+                                    child: TextField(
+                                        keyboardType: TextInputType.emailAddress,
+                                        controller: _controllerNome,
+                                        decoration: InputDecoration(
+                                          hintText: "Nome",
+                                          labelText: "Nome",
+                                          suffixIcon: Icon(Icons.person_outline)
+                                        ),
+                                      ),
+                                 ),
+
+
+
+
+                                // Caixa email
+                                TextField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  controller: _controllerEmail,
+                                  decoration: InputDecoration(
+                                    hintText: "Email",
+                                    labelText: "Email",
+                                    suffixIcon: Icon(Icons.email_outlined)
+                                  ),
+                                ),
+
+
+                                // Caixa Senha
+                                TextField(
+                                  keyboardType: TextInputType.text,
+                                  controller: _controllerSenha,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    hintText: "Senha",
+                                    labelText: "Senha",
+                                    suffixIcon: Icon(Icons.lock_outlined)
+                                  ),
+                                ),
+
+                                SizedBox(height: 20,),
+
+                                Container(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                  onPressed: (){}, 
+                                  style: ElevatedButton.styleFrom(
+                                    primary: PaletaCores.corPrimaria,
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 8),
+                                    child: Text(
+                                     _cadastroUsuario ? "Cadastrar" 
+                                     :  "Entrar"
+                                      ),
+                                    ),
+                                    ),
+                                ),
+
+                                Row(
+                                  children: [
+                                    Text("Login"),
+                                    Switch(
+                                      value: _cadastroUsuario , 
+                                      onChanged: (bool valor){
+                                        setState(() {
+                                          _cadastroUsuario = valor;
+                                        }
+                                        );
+                                      }
+                                      ),
+                                    Text("Cadastro"),
+                                  ],
+                                ),
+
+           
+                              ],
+                            ),
+                          ),
+                          ),
+                  ),
+                ),)
+            ],
+          ),
+        ),
     );
   }
 }
